a93a1986ad4116c91eec6cfaea7078612a67d713
arvind7kuntal <arvind7kuntal@gmail.com>
2026-03-21 15:12:50 +0530

    Day 1: match-ingestion-service setup with Spring Boot 3.5.12

---
+++ match-ingestion-service/src/main/java/com/cricketpulse/matchingestion/MatchIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cricketpulse.matchingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ match-ingestion-service/src/test/java/com/cricketpulse/matchingestion/MatchIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricketpulse.matchingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
